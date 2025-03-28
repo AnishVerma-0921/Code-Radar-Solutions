8c33a0179aed5c47e5f1fd08b160b071813bf6a1
AnishVerma-0921 <anishsunny0001@gmail.com>
2025-03-28 20:39:05 +0530

    Solution to Count Elements Equal to a Given Value

--- Count Elements Equal to a Given Value@@676c02002dbc8c3e7a280b8d/code.c
+++ Count Elements Equal to a Given Value@@676c02002dbc8c3e7a280b8d/code.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 
-int main() {
-    int arr[100], n, K, count = 0;
-    
-    // Input the number of elements
-    
-    scanf("%d", &n);
-    
-    // Input array elements
-    printf("%d", n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    
-    // Input the value to search for
-    
-    scanf("%d", &K);
-    
-    // Count occurrences of K
-    for(int i = 0; i < n; i++) {
-        if(arr[i] == K) {
+int countValue(int array[], int size, int value) {
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        if(array[i] == value) {
             count++;
         }
     }
+    return count;
+}
+
+int main() {
+    int arr[100], n, K;
     
-    // Print the result
-    printf("%d%d ", K, count);
+    printf("Enter number of elements (1-100): ");
+    scanf("%d", &n);
+    
+    if(n < 1 || n > 100) {
+        printf("Invalid input! Size must be 1-100.\n");
+        return 1;
+    }
+    
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        printf("Element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Enter value to count: ");
+    scanf("%d", &K);
+    
+    int occurrences = countValue(arr, n, K);
+    printf("The value %d appears %d time(s).\n", K, occurrences);
     
     return 0;
 }
